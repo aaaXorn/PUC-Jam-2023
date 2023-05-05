d652793182f6066e29afe0f23d1f060264511d15
aaaXorn <lcrs.rp@gmail.com>
2023-05-05 15:54:46 -0300

    aleatorização de eventos

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,12 +7,88 @@ public class GameManager : MonoBehaviour
     public static GameManager s_singleton {get; private set;}
 
     //eventos que ainda não foram usados
+    [SerializeField]
     private List<GameObject> list_unusedEvents = new List<GameObject>();
     //eventos que já foram usados
     private List<GameObject> list_usedEvents = new List<GameObject>();
+
+    private GameObject currEvent;
+
+    #region timer
+    private bool stopTimer = false;
+
+    private bool isInEvent = false;
+    private float eventTime = 0f;
+    private float eventCD = 15f;
+
+    private float gameOverTime = 0f;
+    private float gameOverTimer = 15f;
 
     private void Awake()
     {
         s_singleton = this;
     }
+
+    private void Update()
+    {
+        if(stopTimer)
+        {
+            
+
+            return;
+        }
+
+        if(!isInEvent)
+        {
+            if(eventTime > eventCD)
+            {
+                StartEvent();
+            }
+
+            eventTime += Time.deltaTime;
+        }
+        else
+        {
+            if(gameOverTime > gameOverTimer)
+            {
+                GameOver();
+            }
+
+            gameOverTime += Time.deltaTime;
+        }
+    }
+    #endregion
+
+    #region events
+    private void StartEvent()
+    {
+        isInEvent = true;
+        gameOverTime = 0f;
+
+        int nextEvent = Random.Range(0, list_unusedEvents.Count);
+
+        currEvent = Instantiate(list_unusedEvents[nextEvent], Vector3.zero, Quaternion.identity);
+        list_unusedEvents.RemoveAt(nextEvent);
+    }
+
+    private void GameOver()
+    {
+        stopTimer = true;
+    }
+
+    private void WinEvent()
+    {
+        Destroy(currEvent);
+
+        isInEvent = false;
+        eventTime = 0f;
+
+        stopTimer = true;
+
+        if(list_unusedEvents.Count <= 0)
+        {
+            //win game
+        }
+    }
+    #endregion
 }
