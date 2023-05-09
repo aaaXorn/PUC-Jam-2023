1be96ae9c6c485989f0bb503867143aa5327c97d
aaaXorn <lcrs.rp@gmail.com>
2023-05-09 17:22:43 -0300

    minigame totem

--- Assets/Scripts/Event/MinigameTotem.cs
+++ Assets/Scripts/Event/MinigameTotem.cs
@@ -6,6 +6,45 @@ namespace nEvent
 {
     public class MinigameTotem : MonoBehaviour
     {
-        
+        private RectTransform rTransf;
+
+        [SerializeField]
+        private GameObject prefab_stone;
+        [SerializeField]
+        private RectTransform rTransf_stoneSpawn;
+
+        private int hits = 0;
+        private int hits_needed = 3;
+
+        private void Awake()
+        {
+            rTransf = GetComponent<RectTransform>();
+        }
+
+        private void Start()
+        {
+            SpawnStone();
+        }
+
+        public void SpawnStone()
+        {
+            StoneMinigameTotem _stone = Instantiate(prefab_stone, rTransf_stoneSpawn).GetComponent<StoneMinigameTotem>();
+            _stone.transform.parent = rTransf;
+
+            _stone.minigame = this;
+        }
+
+        public void Hit()
+        {
+            hits++;
+            if(hits >= hits_needed)
+            {
+                GameManager.s_singleton.WinEvent();
+            }
+            else
+            {
+                SpawnStone();
+            }
+        }
     }
 }
--- Assets/Scripts/Event/StoneMinigameTotem.cs
+++ Assets/Scripts/Event/StoneMinigameTotem.cs
@@ -1,59 +1,92 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using nInput;
 
 namespace nEvent
 {
     public class StoneMinigameTotem : MonoBehaviour
     {
-        private enum State { Positioning, Falling, Idle }
-        private State state = State.Positioning;
+        public MinigameTotem minigame;
+
+        private RectTransform rTransf;
+        private Rigidbody2D rb;
+
+        private bool isPositioning = true;
+
+        private bool goRight = true;
+        [SerializeField]
+        private float positioning_spd = 310f;
+        [SerializeField]
+        private float positioning_borderL = -210f, positioning_borderR = 210f;
+
+        [SerializeField]
+        private float falling_spd = 150f;
+
+        private void Awake()
+        {
+            rTransf = GetComponent<RectTransform>();
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            rb.velocity = Vector2.right * positioning_spd;
+        }
 
         private void Update()
         {
-            switch(state)
+            if(isPositioning)
             {
-                case State.Positioning:
-                    StatePositioning();
-                    break;
+                if(InputSP.s_singleton.input_interactDown)
+                {
+                    isPositioning = false;
+                    rb.velocity = Vector2.down * falling_spd;
+                }
+                else
+                {
+                    if(goRight)
+                    {
+                        //rTransf.anchoredPosition += Vector2.right * positioning_spd * Time.deltaTime;
 
-                case State.Falling:
-                    StateFalling();
-                    break;
-                
-                case State.Idle:
-                    StateIdle();
-                    break;
+                        if(rTransf.anchoredPosition.x >= positioning_borderR)
+                        {
+                            goRight = false;
+                            rb.velocity = Vector2.left * positioning_spd;
+                        }
+                    }
+                    else
+                    {
+                        //rTransf.anchoredPosition += Vector2.left * positioning_spd * Time.deltaTime;
+
+                        if(rTransf.anchoredPosition.x <= positioning_borderL)
+                        {
+                            goRight = true;
+                            rb.velocity = Vector2.right * positioning_spd;
+                        }
+                    }
+                }
             }
         }
 
-        private void StatePositioning()
+        private void OnCollisionEnter2D(Collision2D other)
         {
-
-        }
-
-        private void StateFalling()
-        {
-
-        }
-
-        private void StateIdle()
-        {
-
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if(other.CompareTag("Minigame"))
+            print("aaa");
+            if(other.gameObject.CompareTag("MinigameTarget"))
             {
-
+                gameObject.tag = "MinigameTarget";
+                other.gameObject.tag = "Untagged";
+                minigame.Hit();
             }
             else
             {
-
+                minigame.SpawnStone();
+                Destroy(gameObject);
             }
 
-            state = State.Idle;
+            rb.velocity = Vector2.zero;
+
+            Destroy(this);
         }
     }
 }
