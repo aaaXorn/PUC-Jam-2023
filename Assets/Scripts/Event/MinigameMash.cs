cee69ba0701713bcd8fd20dc397c171138910983
aaaXorn <lcrs.rp@gmail.com>
2023-05-11 18:08:05 -0300

    personagem completo, UI minigames mashing e timing

--- Assets/Scripts/Event/MinigameMash.cs
+++ Assets/Scripts/Event/MinigameMash.cs
@@ -13,9 +13,16 @@ namespace nEvent
         [SerializeField]
         private float barra_increasePerTap = 15f;
         [SerializeField]
-        private float barra_targetHeight = 500f;
+        private float barra_targetWidth = 500f;
 
         private Vector2 initPos;
+
+        private float bucket_icon_timer = 0f;
+        [SerializeField]
+        private float bucket_icon_dur = 0.15f;
+
+        [SerializeField]
+        private GameObject obj_bucket_water;
 
         private void Start()
         {
@@ -25,20 +32,30 @@ namespace nEvent
         private void OnDisable()
         {
             rTransf_barra.anchoredPosition = initPos;
-            rTransf_barra.sizeDelta = new Vector2(rTransf_barra.sizeDelta.x, 0f);
+            rTransf_barra.sizeDelta = new Vector2(0f, rTransf_barra.sizeDelta.x);
         }
 
         private void Update()
         {
+            if(obj_bucket_water.activeSelf)
+            {
+                bucket_icon_timer -= Time.deltaTime;
+
+                if(bucket_icon_timer <= 0f) obj_bucket_water.SetActive(false);
+            }
+
             if(InputSP.s_singleton.input_interactDown)
             {
-                rTransf_barra.anchoredPosition += Vector2.up * barra_increasePerTap / 2f;
-                rTransf_barra.sizeDelta += Vector2.up * barra_increasePerTap;
+                bucket_icon_timer = bucket_icon_dur;
+                if(!obj_bucket_water.activeSelf) obj_bucket_water.SetActive(true);
 
-                if(rTransf_barra.anchoredPosition.y >= 0f)
+                rTransf_barra.anchoredPosition += Vector2.right * barra_increasePerTap / 2f;
+                rTransf_barra.sizeDelta += Vector2.right * barra_increasePerTap;
+
+                if(rTransf_barra.anchoredPosition.x >= 0f)
                 {
-                    rTransf_barra.anchoredPosition = new Vector2(rTransf_barra.anchoredPosition.x, 0f);
-                    rTransf_barra.sizeDelta = new Vector2(rTransf_barra.sizeDelta.x, barra_targetHeight);
+                    rTransf_barra.anchoredPosition = new Vector2(0f, rTransf_barra.anchoredPosition.x);
+                    rTransf_barra.sizeDelta = new Vector2(barra_targetWidth, rTransf_barra.anchoredPosition.y);
 
                     GameManager.s_singleton.WinEvent();
                 }
--- Assets/Scripts/Event/MinigameTiming.cs
+++ Assets/Scripts/Event/MinigameTiming.cs
@@ -11,26 +11,29 @@ namespace nEvent
         private RectTransform rTransf_bolinha;
         
         [SerializeField]
-        private float bolinha_spd = 450f;
+        private float bolinha_spd = 920f;
         [SerializeField]
-        private float bolinha_border_top = 233f, bolinha_border_bottom = -233f;
+        private float bolinha_border_top = 460f, bolinha_border_bottom = -460f;
         [SerializeField]
-        private float bolinha_target_top = 34f, bolinha_target_bottom = -34f;
+        private float bolinha_target_top = 75f, bolinha_target_bottom = -75f;
+        [SerializeField]
+        private float bolinha_sourSpot_top = 195f, bolinha_sourSpot_bottom = -195f;
 
         private bool bolinha_goUp = true;
 
         private int points = 0;
         [SerializeField]
-        private int points_needed = 5;
+        private int points_needed = 100;
         [SerializeField]
-        private int points_perHit = 1, points_perMiss = 2;
+        private int points_perHit = 20, points_perMiss = 40, points_perSourSpot = 5;
 
         private Vector2 bolinha_initPos;
 
         [SerializeField]
         private RectTransform rTransf_barra;
         private Vector2 barra_initPos;
-        private float barra_targetWidth = 300f;
+        [SerializeField]
+        private float barra_targetWidth = 953f;
 
         private void Start()
         {
@@ -78,7 +81,11 @@ namespace nEvent
             {
                 if(rTransf_bolinha.anchoredPosition.y <= bolinha_target_top && rTransf_bolinha.anchoredPosition.y >= bolinha_target_bottom)
                 {
-                    TimingHit();
+                    TimingHit(true);
+                }
+                else if(rTransf_bolinha.anchoredPosition.y <= bolinha_sourSpot_top && rTransf_bolinha.anchoredPosition.y >= bolinha_sourSpot_bottom)
+                {
+                    TimingHit(false);
                 }
                 else
                 {
@@ -87,9 +94,9 @@ namespace nEvent
             }
         }
 
-        private void TimingHit()
+        private void TimingHit(bool _sweetSpot)
         {
-            points += points_perHit;
+            points += (_sweetSpot ? points_perHit : points_perSourSpot);
 
             if(points >= points_needed)
             {
@@ -99,8 +106,6 @@ namespace nEvent
             
             rTransf_barra.sizeDelta = new Vector2 (barra_targetWidth * points / points_needed, rTransf_barra.sizeDelta.y);
             rTransf_barra.anchoredPosition = barra_initPos + new Vector2(rTransf_barra.sizeDelta.x/2, 0f);
-
-            print("hit");
         }
 
         private void TimingMiss()
@@ -108,8 +113,6 @@ namespace nEvent
             points -= points_perMiss;
 
             if(points < 0) points = 0;
-
-            print("miss");
         }
     }
 }
