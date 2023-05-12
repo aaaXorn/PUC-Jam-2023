3f63379896c7797a75931794a18abfab09d63dcc
aaaXorn <lcrs.rp@gmail.com>
2023-05-12 16:14:03 -0300

    shake durante evento vulcão

--- Assets/Scripts/Camera/CameraSimple3D.cs
+++ Assets/Scripts/Camera/CameraSimple3D.cs
@@ -15,12 +15,25 @@ namespace nCamera
         [SerializeField]
         protected Vector3 camOffset = new Vector3(0f, 10f, -5f);
 
+        public float shake = 0f;
+
+        protected Vector3 shake_vector = Vector3.zero;
+
         protected void Start()
         {
             if(camFollow == null)
             {
                 camFollow = Camera.main.transform;
             }
+        }
+
+        protected void FixedUpdate()
+        {
+            float shake_x = Random.Range(0, shake);
+            float shake_y = Random.Range(0, shake);
+            float shake_z = Random.Range(0, shake);
+
+            shake_vector = new Vector3(shake_x, shake_y, shake_z);
         }
 
         protected void LateUpdate()
@@ -31,12 +44,21 @@ namespace nCamera
 
         protected void MoveCameraFollow()
         {
-            camFollow.position = playerFollow.position;
+            float shake_x = Random.Range(0, shake);
+            float shake_y = Random.Range(0, shake);
+            float shake_z = Random.Range(0, shake);
+
+            camFollow.position = playerFollow.position + shake_vector;
         }
 
         protected void MoveCamera()
         {
             transform.position = camFollow.position + camOffset;
+        }
+
+        public void Shake(bool _shake)
+        {
+            shake = _shake ? 0.05f : 0f;
         }
     }
 }
